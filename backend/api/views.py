425df42b66baff6ba6fa3bf2fd7fd3b7291d9751
bobr <borya2072@gmail.com>
2023-03-25 21:20:27 +0300

    ,,,

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import (SAFE_METHODS, IsAuthenticated)
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
 
 from recipes.models import (Cart, Favorite, Ingredient, Recipe,
